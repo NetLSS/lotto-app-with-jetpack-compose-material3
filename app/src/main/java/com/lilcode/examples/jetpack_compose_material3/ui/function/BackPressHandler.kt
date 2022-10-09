3845a9f138e4b9e9074789964ec5e2ca96d241d3
NetLSS <futureprogrammer@naver.com>
2022-10-09 20:36:00 +0900

    Feat: open lottery page singleTop

---
+++ app/src/main/java/com/lilcode/examples/jetpack_compose_material3/ui/function/BackPressHandler.kt
@@ -0,0 +1,34 @@
+package com.lilcode.examples.jetpack_compose_material3.ui.function
+
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.runtime.*
+
+/**
+ * cf. [origin](https://www.valueof.io/blog/intercept-back-press-button-in-jetpack-compose)
+ */
+@Composable
+fun BackPressHandler(
+    backPressedDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backPressedDispatcher) {
+        backPressedDispatcher?.addCallback(backCallback)
+
+        onDispose {
+            backCallback.remove()
+        }
+    }
+}
